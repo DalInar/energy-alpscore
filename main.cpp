a38719637923356c403ff30efa25940d3cea3d60
Joe <oryx@umich.edu>
2016-07-18 17:27:54 -0400

    Code is all written! Now we need to test

--- main.cpp
+++ main.cpp
@@ -15,6 +15,14 @@ typedef alps::gf::index spin_index;
 
 void read_iteration(int iteration, const std::string &directory, const std::string &GF_h5_filename, cluster_matsubara_kspace_gf &green,
                     cluster_matsubara_kspace_gf &sigma, std::vector<double> &density);
+void print_tails(cluster_matsubara_kspace_gf green);
+void calculate_density(int nfreq, int nsite, double beta, cluster_matsubara_kspace_gf &green,
+                       cluster_matsubara_kspace_gf &sigma, std::vector<double> & density);
+void calculate_green_sigma_ave(int M, std::vector<cluster_matsubara_kspace_gf> greens, std::vector<cluster_matsubara_kspace_gf> sigmas,
+                               cluster_matsubara_kspace_gf &green_ave, cluster_matsubara_kspace_gf &sigma_ave, bool partial, int partial_index);
+void compute_energy(double &kin, double &pot, int nfreq, double  beta, double U, int nsite,
+                    cluster_matsubara_kspace_gf &green, cluster_matsubara_kspace_gf &sigma);
+void print_head_gf(cluster_matsubara_kspace_gf green);
 
 int main(int argc, char **argv) {
     std::cout << "Calculating Energy from Self-Energy and Matsubara GF" << std::endl;
@@ -26,7 +34,7 @@ int main(int argc, char **argv) {
     try{    //command line parser
         TCLAP::CmdLine cmd("Calculate Energy from Matsubara Self Energies and Greens functions");
         TCLAP::ValueArg<std::string> directory_arg("d", "directory", "directory containing SE, GF (.)", false, ".", "string", cmd);
-        TCLAP::ValueArg<std::string> GF_filename_arg("G", "greens_h5", "Greens function h5 base filename (greens)", false, "greens_h5_", "string", cmd);
+        TCLAP::ValueArg<std::string> GF_filename_arg("G", "greens_h5", "Greens function h5 base filename (greens)", false, "Greens_", "string", cmd);
         TCLAP::ValueArg<std::string> h5_filename_arg("x", "hdf5", "hdf5 file with parameters (sim.h5)", false, "sim.h5", "string", cmd);
         TCLAP::ValueArg<int> niteration_arg("n", "niteration", "number of iterations (0)", false, 0, "int", cmd);
         TCLAP::ValueArg<int> siteration_arg("s", "siteration", "starting iteration for energy calculation (0)", false, 0, "int", cmd);
@@ -57,6 +65,7 @@ int main(int argc, char **argv) {
     }
 
     int num_iterations = niteration - siteration + 1;
+    std::cout<<"Num Iterations = "<<num_iterations<<std::endl;
 
     //Get parameters from simulation h5 file
     std::stringstream h5_file_fullpath; h5_file_fullpath << directory<< "/" <<Sim_h5_filename;
@@ -74,6 +83,7 @@ int main(int argc, char **argv) {
     std::cout<<"beta = "<<beta<<std::endl;
     ar.close();
 
+    //Dummy gf just so we can initialize vector
     cluster_matsubara_kspace_gf dummy_gf(alps::gf::omega_k_sigma_gf(
             alps::gf::matsubara_positive_mesh(beta, nfreq),
             alps::gf::momentum_index_mesh(9,2),
@@ -90,16 +100,145 @@ int main(int argc, char **argv) {
         read_iteration(i, directory, GF_h5_filename, green[i-siteration], sigma[i-siteration], density[i-siteration]);
     }
 
+    /*
+     * Prepare the jackknife quantities
+     */
+    cluster_matsubara_kspace_gf template_gf(green[0]);    //Copy one of the read GF, get correct meshes
+    template_gf.initialize();  //Set entries to zero
+    std::vector<cluster_matsubara_kspace_gf> sigma_i(num_iterations, template_gf);
+    cluster_matsubara_kspace_gf sigma_zero(template_gf);
+    cluster_matsubara_kspace_gf sigma_bar(template_gf);
+
+    std::vector<cluster_matsubara_kspace_gf> green_i(num_iterations, template_gf);
+    cluster_matsubara_kspace_gf green_zero(template_gf);
+    cluster_matsubara_kspace_gf green_bar(template_gf);
+
+   // std::vector<std::vector<double> > density_i(num_iterations, std::vector<double>(nsites*2, 0.5));
+   // std::vector<double> density_zero(nsites*2, 0.5);
+   // std::vector<double> density_bar(nsites*2, 0.5);
+
+    std::cout<<"Sigma_i "<<0<<std::endl;
+    print_tails(sigma_i[0]);
+
+    /*
+     * Calculate each of the needed jackknife quantities
+     */
+    calculate_green_sigma_ave(num_iterations, green, sigma, green_zero, sigma_zero, false, 0);
+    for(int z=0; z<num_iterations; ++z){
+        calculate_green_sigma_ave(num_iterations, green, sigma, green_i[z], sigma_i[z], true, z);
+    }
+    calculate_green_sigma_ave(num_iterations, green_i, sigma_i, green_bar, sigma_bar, false, 0);
+
+    print_head_gf(green[0]);
+    print_head_gf(sigma[0]);
+    print_head_gf(sigma_zero);
+    print_head_gf(sigma_i[0]);
+    print_head_gf(sigma_bar);
+
+    /*
+     * Compute the potential and kinetic energies
+     */
+    std::vector<double> ekin_i(num_iterations);
+    std::vector<double> epot_i(num_iterations);
+    double ekin_zero, ekin_bar;
+    double epot_zero, epot_bar;
+    for(int z=0;z<num_iterations;++z){
+        compute_energy(ekin_i[z], epot_i[z], nfreq,  beta, U, nsites, green_i[z], sigma_i[z]);
+    }
+    compute_energy(ekin_zero, epot_zero, nfreq,  beta, U, nsites, green_zero, sigma_zero);
+    compute_energy(ekin_bar, epot_bar, nfreq,  beta, U, nsites, green_bar, sigma_bar);
+
+    /***********************************/
+    //Jackknife mean and error
+    /***********************************/
+    double ekin_mean=ekin_zero-(num_iterations-1.)*(ekin_bar-ekin_zero);
+    double epot_mean=epot_zero-(num_iterations-1.)*(epot_bar-epot_zero);
+    double ekin_isquare=0.;
+    double epot_isquare=0.;
+    for(int z=0;z<num_iterations;++z){
+        ekin_isquare+=ekin_i[z]*ekin_i[z];
+        epot_isquare+=epot_i[z]*epot_i[z];
+    }
+    //std::cout<< ekin_isquare<< " "<< M<< " " << ekin_bar<< " "<<ekin_isquare/M-ekin_bar*ekin_bar<< std::endl;
+    //std::cout<< epot_isquare<< " "<< M<< " " << epot_bar<< " "<<epot_isquare/M-epot_bar*epot_bar<< std::endl;
+    double ekin_error=std::sqrt(num_iterations-1.)*std::sqrt(fabs(ekin_isquare/num_iterations-ekin_bar*ekin_bar));
+    double epot_error=std::sqrt(num_iterations-1.)*std::sqrt(fabs(epot_isquare/num_iterations-epot_bar*epot_bar));
+
+    std::cout<<1.0/nfreq<<" "<<ekin_mean<<" "<<ekin_error<<" "<<epot_mean<<" "<<epot_error<<" "<<ekin_mean+epot_mean<<std::endl;
+
+
     return 0;
 }
 
-void read_iteration(int iteration, const std::string &directory, const std::string &GF_h5_filename, cluster_matsubara_kspace_gf &green,
-                    cluster_matsubara_kspace_gf &sigma, std::vector<double> &density){
-    std::stringstream g_filename; g_filename << directory<< "/" <<GF_h5_filename<<iteration<<".h5";
-    alps::hdf5::archive ar(g_filename.str().c_str(), alps::hdf5::archive::READ);
+void calculate_green_sigma_ave(int M, std::vector<cluster_matsubara_kspace_gf> greens, std::vector<cluster_matsubara_kspace_gf> sigmas,
+                    cluster_matsubara_kspace_gf &green_ave, cluster_matsubara_kspace_gf &sigma_ave, bool partial, int partial_index){
+    std::cout<<"Gathering tails"<<std::endl;
+    //Get tails
+    cluster_matsubara_kspace_gf_tail green_c1 = greens[0].tail(1);
+    cluster_matsubara_kspace_gf_tail green_c2 = greens[0].tail(2);
+    cluster_matsubara_kspace_gf_tail green_c3 = greens[0].tail(3);
+    green_c1.initialize();
+    green_c2.initialize();
+    green_c3.initialize();
 
-    green.load(ar, "/G_omega");  
-    sigma.load(ar, "/Sigma_omega");
+    cluster_matsubara_kspace_gf_tail sigma_c0 = sigmas[0].tail(0);
+    cluster_matsubara_kspace_gf_tail sigma_c1 = sigmas[0].tail(1);
+    sigma_c0.initialize();
+    sigma_c1.initialize();
+
+    green_ave.initialize();
+    sigma_ave.initialize();
+
+    double N = (partial ? M-1 : M);
+    if(int(N) == 0){
+        std::cout<<"Cannot calculate partial for only one iteration, returning zero"<<std::endl;
+        std::cout<<"Setting blank tails"<<std::endl;
+        green_ave.set_tail(1, green_c1);    sigma_ave.set_tail(0, sigma_c0);
+        green_ave.set_tail(2, green_c2);    sigma_ave.set_tail(1, sigma_c1);
+        green_ave.set_tail(3, green_c3);
+        return;
+    }
+    std::cout<<"Calculating aves"<<std::endl;
+    for(int z=0; z<M; ++z){
+        if(partial && partial_index == z) {
+            continue;
+        }
+        green_ave += greens[z];
+        sigma_ave += sigmas[z];
+
+        green_c1 += greens[z].tail(1);
+        green_c2 += greens[z].tail(2);
+        green_c3 += greens[z].tail(3);
+
+        sigma_c0 += sigmas[z].tail(0);
+        sigma_c1 += sigmas[z].tail(1);
+    }
+
+    green_ave /= N;
+    sigma_ave /= N;
+
+    green_c1 /= N;  sigma_c0 /= N;
+    green_c2 /= N;  sigma_c1 /= N;
+    green_c3 /= N;
+
+    std::cout<<"Setting tails"<<std::endl;
+    green_ave.set_tail(1, green_c1);    sigma_ave.set_tail(0, sigma_c0);
+    green_ave.set_tail(2, green_c2);    sigma_ave.set_tail(1, sigma_c1);
+    green_ave.set_tail(3, green_c3);
+
+}
+
+void print_head_gf(cluster_matsubara_kspace_gf green){
+    freq_index freq(0);
+    for(mom_index k(0); k<green.mesh2().extent(); ++k){
+        for(spin_index s(0); s<green.mesh3().extent(); ++s){
+            std::cout << green(freq, k, s)<<"  ";
+        }
+    }
+    std::cout<<std::endl;
+}
+
+void print_tails(cluster_matsubara_kspace_gf green){
     std::vector <cluster_matsubara_kspace_gf_tail> tail;
     tail=green.tail();
     std::cout<<"Num coeffs = "<<tail.size()<<std::endl;
@@ -122,8 +261,43 @@ void read_iteration(int iteration, const std::string &directory, const std::stri
             }
             std::cout<<std::endl;
         }
-
     }
+}
+
+void read_iteration(int iteration, const std::string &directory, const std::string &GF_h5_filename, cluster_matsubara_kspace_gf &green,
+                    cluster_matsubara_kspace_gf &sigma, std::vector<double> &density){
+    std::stringstream g_filename; g_filename << directory<< "/" <<GF_h5_filename<<iteration<<".h5";
+    alps::hdf5::archive ar(g_filename.str().c_str(), alps::hdf5::archive::READ);
+
+    green.load(ar, "/G_komega");
+    sigma.load(ar, "/Sigma_komega");
+    std::cout<<"Tails for Green "<<iteration<<std::endl;
+    print_tails(green);
+    std::cout<<"Tails for Sigma "<<iteration<<std::endl;
+    print_tails(sigma);
+}
+
+void calculate_density(int nfreq, int nsite, double beta, cluster_matsubara_kspace_gf &green,
+                       cluster_matsubara_kspace_gf &sigma, std::vector<double> & density){
+    double hifreq_integral_square=beta*beta*gsl_sf_psi_n (1, 0.5+nfreq)/(4.*M_PI*M_PI); //this takes care of the 1/omega^2 part
+    cluster_matsubara_kspace_gf_tail green_c2 = green.tail(2);
+    for(mom_index i(0);i<nsite;++i){
+        for(spin_index f(0);f<2;++f){
+            for(freq_index w(0);w<nfreq;++w){
+                density[f()*nsite+i()]+=2.*green(w,i,f).real()/beta;
+            }
+            density[f()*nsite+i()]-=2.*hifreq_integral_square*green_c2(i,f)/beta;
+        }
+    }
+    //real space density for Hartree term
+    double density_up=0., density_dn=0.;
+    for(int i=0;i<nsite;++i){
+        density_up+=density[      i]/(double)nsite;
+        density_dn+=density[nsite+i]/(double)nsite;
+    }
+
+    std::cout<<"Density Up = "<<density_up<<std::endl;
+    std::cout<<"Density Down = "<<density_dn<<std::endl;
 }
 
 void compute_energy(double &kin, double &pot, int nfreq, double  beta, double U, int nsite,
